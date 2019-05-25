c378b7fe1725446c12226d4e9d286b3e9a44ad2a
Dano <dano@dcolombo.com>
2019-05-24 21:03:32 -0400

    db connect troubleshooting

--- configs/config.php
+++ configs/config.php
@@ -2,7 +2,8 @@
   $host       = "ls-56c3ce21a2ca831ec15ce9ccccd476b62df8d789.c2cwx5xdgogr.us-east-1.rds.amazonaws.com";
   $username   = "dbmaster";
   $password   = "Tw0Zer019R0mans1212!";
-  $dsn        = "mysql:host=$host;dbname=p8rally";
+  $dbname = "p8rally";
+  $dsn        = "mysql:host=$host;dbname=$dbname";
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
                 );
